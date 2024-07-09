7cba447003989932410a2fe092f50c017745bd84
Daniel Alemán <dan13laleman14@gmail>
2024-07-08 21:26:35 -0300

    nuevo register.handlebars

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -1,45 +1,40 @@
 const form = document.getElementById("registerForm");
-const errorMessage = document.getElementById("errorMessage"); 
+const errorMessage = document.getElementById("errorMessage");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const data = new FormData(form);
-  const obj = {};
-  data.forEach((value, key) => (obj[key] = value));    
-  
-  fetch("/api/users/register", {
-    method: "POST",
-    body: JSON.stringify(obj),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-  .then(response => {
-    if (response.status === 200) {
-        return response.json();
-    } else {
-        errorMessage.textContent = 'Este email ya es un usuario. Logueate'; 
-        errorMessage.style.display = 'block';
-        throw new Error('Error al registrarse');
-    }
-  })
-  .then(data => {
-    const token = data.access_token;
-    const userId = data.userId;
-    const userRole = data.userRole;
+    e.preventDefault();
+    const data = new FormData(form);
 
-    localStorage.setItem('token', token);
-    localStorage.setItem('userId', userId);
-    localStorage.setItem('userRole', userRole)
+    fetch("/api/users/register", {
+        method: "POST",
+        body: data,
+    })
+    .then(response => {
+        if (response.status === 200) {
+            return response.json();
+        } else {
+            errorMessage.textContent = 'Este email ya es un usuario. Logueate';
+            errorMessage.style.display = 'block';
+            throw new Error('Error al registrarse');
+        }
+    }) 
+    .then(data => {
+        const token = data.access_token;
+        const userId = data.userId;
+        const userRole = data.userRole;
 
-    console.log("Token:", token);
-    console.log("Registro exitoso!");
-    console.log("User Id:", userId);
-    console.log("user rol:", userRole)
+        localStorage.setItem('token', token);
+        localStorage.setItem('userId', userId);
+        localStorage.setItem('userRole', userRole)
 
-    window.location.href = "http://localhost:8080/api/products/"; 
-  })
-  .catch(error => {
-    console.error('Error en el registro:', error);
-  });
+        console.log("Token:", token);
+        console.log("Registro exitoso!");
+        console.log("User Id:", userId);
+        console.log("user rol:", userRole)
+
+        window.location.href = "http://localhost:8080/api/products/";
+    })
+    .catch(error => {
+        console.error('Error en el registro:', error);
+    });
 });
